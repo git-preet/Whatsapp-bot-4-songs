4552976914785fd2a2d6e94832680ad2d78f5813
PREET <68598635+git-preet@users.noreply.github.com>
2024-07-11 11:35:46 -0400

    Create language.ts

---
+++ src/language.ts
@@ -0,0 +1,25 @@
+const br = {
+  CONNECTED: 'Conectado com sucesso!',
+  DOWNLOAD_STARTED: '*INICIANDO DOWNLOAD:* _Esta ação pode demorar um pouco!_',
+  ERROR: 'Erro ao concluir a operação.',
+  SEARCHING_FOR: '*Procurando por*',
+  FOUNDED: '*Encontrado*',
+  AVAILABLE_COMMANDS: '*Comandos disponíveis:*',
+  HELP_PLAY:
+    '*Comando:* play\n*Descrição:* Reproduz o áudio de algum vídeo do youtube',
+  HELP_HELP: '*Comando:* help\n*Descrição:* Exibe esta mensagem',
+};
+
+const en = {
+  CONNECTED: 'Successfully connected!',
+  DOWNLOAD_STARTED: '*DOWNLOAD STARTED:* _This action can take some minutes_',
+  ERROR: 'Error to complete operation.',
+  SEARCHING_FOR: '*Searching for*',
+  FOUNDED: '*Founded*',
+  AVAILABLE_COMMANDS: '*Available commands:*',
+  HELP_PLAY:
+    '*Command:* play\n*Description:* Play the audio of a youtube video',
+  HELP_HELP: '*Command:* help\n*Description:* Show this message',
+};
+
+export default { br, en };
